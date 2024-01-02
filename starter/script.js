d4d4dba1f5d9777e7288a1553bdd868f04b09c0a
jeyabdo <joephoenix755@gmail.com>
2024-01-02 19:09:06 +0000

    object in javascript

---
+++ starter/script.js
@@ -0,0 +1,16 @@
+//WRITE YOUR CODE BELOW
+ let customerOrder = {
+    drink: "coffee" , 
+    sugarAmount: 2,
+    orderReady : false
+
+ };
+
+ if( customerOrder.orderReady){
+    console.log("Ready for pick-up");
+ }
+ else {
+    console.log("Still in order queue");
+
+ }
+ 
