b12f53d8bef04c813ddbfbacf8b73dad94992510
jeyabdo <joephoenix755@gmail.com>
2024-01-02 09:12:12 +0000

    added variable

--- Task-01/script.js
+++ Task-01/script.js
@@ -1,1 +1,24 @@
+// program to guess a number 
 
+let num = Math.floor(Math.random() *10 + 1);
+
+let guesses = 0;
+
+document.getElementById("submitbtn").onclick = function() {
+
+  let guess = document.getElementById("fieldtext").value;
+  guesses++;
+
+  if (guess == num){
+    alert(`you got correct the number is ${num}`);
+  }
+  else if(guess < num) {
+    alert(`the number is lower`);
+
+  }
+  else{
+    alert(`the number is higher`);
+
+  }
+
+}
